7e3e55b14c9db096ae962cc59acc8a2b67fd9503
FrancoisCAN <francois.cancalon@gmail.com>
2025-07-16 16:39:40 +0200

    feat: add basic homepage

---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// region Components
+import MoonIcon from '@/components/ui/icons/MoonIcon.vue'
+import SunIcon from '@/components/ui/icons/SunIcon.vue'
+// endregion
+</script>
+
+<template>
+  <header class="navbar fixed px-4 pb-3 pt-3 top-0 right-0 left-0 bg-base-100 shadow-xl">
+    <div class="flex-1">
+      <label class="swap swap-rotate">
+        <!-- Switch between themes -->
+        <input class="theme-controller" type="checkbox" value="business" />
+        <SunIcon class="swap-off h-8 w-8 fill-current" />
+        <MoonIcon class="swap-on h-8 w-8 fill-current" />
+      </label>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li>
+          <a>A propos</a>
+        </li>
+        <li>
+          <a>Projets</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// region Assets
+import profilImage from '@/assets/images/profil.jpg'
+// endregion
+
+// region Components
+import GithubIcon from '@/components/ui/icons/GithubIcon.vue'
+import LinkedinIcon from '@/components/ui/icons/LinkedinIcon.vue'
+import NavigationMenu from '@/components/NavigationMenu.vue'
+// endregion
+</script>
 
 <template>
-  <main class="home-view">
-    <div class="inline-grid *:[grid-area:1/1]">
-      <div class="status status-error animate-ping"></div>
-      <div class="status status-error"></div>
+  <NavigationMenu />
+  <main class="flex flex-col h-dvh">
+    <div class="flex flex-col items-center justify-center h-full w-full">
+      <img
+        alt="Photo de profil"
+        class="mask mask-hexagon h-[10rem] mb-12 grayscale"
+        :src="profilImage"
+      />
+      <h1 class="font-secondary text-5xl">Francois Cancalon</h1>
+      <div class="divider mx-auto w-[75%]"></div>
+      <h2 class="text">Développeur d'application web et mobile</h2>
     </div>
-    Server is down
+    <div class="flex p-4 bg-neutral">
+      <div class="flex flex-1">
+        <GithubIcon class="h-6 w-6 mr-8 fill-base-300"/>
+        <LinkedinIcon class="h-6 w-6 fill-base-300"/>
+      </div>
+      <div class="flex-none">
+        <span class="text-base-300">Lyon</span>
+      </div>
+    </div>
   </main>
 </template>
